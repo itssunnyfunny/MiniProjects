1c5ccf37d7c3306ad0b616eb94814e611e5d1ad4
itssunnyfunny <sunnyfunny4062@gmail.com>
2024-12-13 14:08:01 +0530

    the main functionality is added the formbuilder

--- week-3/medium/Form-Builder/index.js
+++ week-3/medium/Form-Builder/index.js
@@ -5,4 +5,42 @@
 // second is for label link to the input field 
 // on clicking the button a new field is added with the previous fields 
 // in second card the new field is added 
-// thats all !
+// thats all 
+
+const fieldType = document.getElementById('input-type');
+const fieldLabel = document.getElementById('input-label');
+const renderField = document.getElementById('form-preview');
+const addBtn = document.getElementById('submit-btn')
+
+
+
+function CreateForm() {
+
+    const inType = fieldType.value;
+    const inLabel = fieldLabel.value;
+
+     const wrapperEl = document.createElement('div');
+     wrapperEl.classList.add('input-card');
+
+     const newInput = document.createElement('input')
+
+     newInput.type = inType;
+     newInput.id = inLabel;
+
+
+     const newLabel = document.createElement('label')
+     newLabel.htmlFor = inLabel
+     newLabel.textContent = inLabel
+     
+     wrapperEl.appendChild(newLabel);
+     
+     wrapperEl.appendChild(newInput);
+
+ 
+     renderField.appendChild(wrapperEl)
+}
+addBtn.addEventListener('click',CreateForm);
+
+
+
+
