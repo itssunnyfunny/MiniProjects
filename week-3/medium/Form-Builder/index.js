8faf5e9cb38b4aa8c591786fa756e5078ec975bb
itssunnyfunny <sunnyfunny4062@gmail.com>
2024-12-13 12:38:20 +0530

    basic structure of the app is done

---
+++ week-3/medium/Form-Builder/index.js
@@ -0,0 +1,8 @@
+
+
+// first create two short of cards fields 
+// in first card you add two input fields first is for input type
+// second is for label link to the input field 
+// on clicking the button a new field is added with the previous fields 
+// in second card the new field is added 
+// thats all !
