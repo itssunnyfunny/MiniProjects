b60bb344261100f1a0ab98c63de391cccebcd753
itssunnyfunny <sunnyfunny4062@gmail.com>
2024-12-15 10:56:45 +0530

    remove all the unneccesory comments

--- week-3/medium/Form-Builder/index.js
+++ week-3/medium/Form-Builder/index.js
@@ -1,12 +1,4 @@
 
-
-// first create two short of cards fields 
-// in first card you add two input fields first is for input type
-// second is for label link to the input field 
-// on clicking the button a new field is added with the previous fields 
-// in second card the new field is added 
-// thats all 
-
 const fieldType = document.getElementById('input-type');
 const fieldLabel = document.getElementById('input-label');
 const renderField = document.getElementById('form-preview');
