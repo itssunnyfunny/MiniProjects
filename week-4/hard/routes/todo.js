b06660ca05c466d1b2efc67691bfac95b4951966
itssunnyfunny <sunnyfunny4062@gmail.com>
2024-12-20 11:50:35 +0530

    route for creating todo is build

--- week-4/hard/routes/todo.js
+++ week-4/hard/routes/todo.js
@@ -1,10 +1,36 @@
 const { Router } = require("express");
 const adminMiddleware = require("../middleware/user");
 const router = Router();
+const {Todo} = require('../database/index')
+
+// toughts
+// don't try to over engineer it
+//  just build what is asked
 
 // todo Routes
-router.post('/', (req, res) => {
-    // Implement todo creation logic
+
+router.post('/', async(req, res) => {
+
+  try {
+    const {title, description} = req.body;
+    
+    if (!title || !description) {
+        return res.status(401).json({
+            message : "provide all the credencials"
+        })
+    };
+
+    const todo =  await Todo.create({title, description});
+    res.json({
+        message: "todo is created",
+        todo
+    })
+  } catch (error) {
+     console.error("Error in creating Todo", error)
+     return res.status(500).json({
+        message: "Internal sever error during todo creation"
+     })
+  }
 });
 
 router.put('/', adminMiddleware, (req, res) => {
