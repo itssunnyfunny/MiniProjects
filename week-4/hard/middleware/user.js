f95cff1f4f2ea749e1bdc7e2bf8041c2e8293bdb
itssunnyfunny <sunnyfunny4062@gmail.com>
2024-12-20 10:45:10 +0530

    user authorization is added

--- week-4/hard/middleware/user.js
+++ week-4/hard/middleware/user.js
@@ -1,5 +1,38 @@
+const jwt = require('jsonwebtoken')
+const dotenv = require("dotenv");
+dotenv.config();
+
+const JWT_PASSWORD = process.env.JWT_PASSWORD;
+
 function userMiddleware(req, res, next) {
-    // Implement user auth logic
+
+ try {
+    const token = req.headers.Authorization;
+
+    if (!token) {
+      return res.status(401).json({
+          message: "token not provided"
+      })
+    };
+  
+  
+    const verify = jwt.verify({token},JWT_PASSWORD);
+  
+    if (!verify) {
+      return res.status(401).json({
+          message: "token is not verified"
+      })
+    } else {
+      next()
+    }
+     
+ } catch (error) {
+    console.error("Error duing user authorization", error)
+   return res.status(500).json({
+    message: "Internal server Error During Authorizations"
+   })
+ }
+
 }
 
 module.exports = userMiddleware;
