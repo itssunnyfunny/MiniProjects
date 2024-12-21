a1408bfe83ba9bc49499c4f05cdc1e5f5f5eb702
itssunnyfunny <sunnyfunny4062@gmail.com>
2024-12-21 12:13:38 +0530

    signup route is created

--- week-5/backend/routes/user.js
+++ week-5/backend/routes/user.js
@@ -1,10 +1,30 @@
 const {Router} = require('express');
+const {User}  = require('../db/index');
 
+const jwt = require('jsonwebtoken');
+require('dotenv').config();
+
+const SECRET = process.env.JWT_SECRET;
 
 const router = Router();
 
 
 router.post('/signup',async (req, res) => {
+    const {username, password} = req.body;
+    try {
+         const user = await User.findOne({username})
+         if (user) {
+            return res.status(403).json({message: "user already exists"});
+         }
+         const newUser = new User({username, password})
+         await newUser();
+
+         const token = jwt.sign({userId : newUser._id},SECRET, {expiresIn:'1h'})
+
+         res.json({message : "user created successfully"},token)
+    } catch (error) {
+        res.json(500).send('Server Error during signup',error)
+    }
     
 })
 
