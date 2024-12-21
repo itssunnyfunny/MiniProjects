7f75ef5e088316ecb83b6288e7adf9c8ffac0ec8
itssunnyfunny <sunnyfunny4062@gmail.com>
2024-12-21 11:29:26 +0530

    schema for the app is created and function to connect DB

--- week-5/backend/db/index.js
+++ week-5/backend/db/index.js
@@ -1,1 +1,38 @@
-//  start writing from here
+
+const mongoose = require('mongoose');
+require('dotenv').config()
+
+ const connectToDatabase = async () => {
+      try {
+    await  mongoose.connect( process.env.MONGO_URI) 
+      console.log("MongoDB connected");
+      
+      } catch (error) {
+        console.error("Error during connecting DB",error)
+        process.exit(1);
+      }
+ }
+
+ const Schema = mongoose.Schema;
+ const ObjectId = Schema.ObjectId;
+
+ const UserSchema = new Schema({
+    username : String,
+    password: String
+ });
+
+ const TodoSchema = new Schema({
+    userId : ObjectId,
+    title: String,
+    description: String
+ })
+
+ const User  = mongoose.model('User',UserSchema);
+ const Todo  = mongoose.model('Todo', TodoSchema);
+
+module.exports({
+    connectToDatabase,
+    User,
+    Todo
+})
+
