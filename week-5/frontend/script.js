37ec254d1faf76ac9e9e6fbd44f36c6ee342d34d
itssunnyfunny <sunnyfunny4062@gmail.com>
2024-12-25 11:12:27 +0530

    sign in feature on the frontend

--- week-5/frontend/script.js
+++ week-5/frontend/script.js
@@ -27,7 +27,34 @@
  })
 
 // signin form handling
+ document.getElementById('signin-form').addEventListener('submit',async (e) => {
+   e.preventDefault();
+ const username =   document.getElementById('signin-username').value;
+ const password =   document.getElementById('signin-password').value;
+ try {
+   const response = await fetch('http//:localhost:3000/signin',{
+      method: 'POST',
+      headers: {
+         'Content-Type' : 'application/json',
+      },
+      body: JSON.stringify({username, password})
+   });
+   const result = await response.json();
 
+   if (response.ok) {
+      localStorage.setItem('token', result.token);
+      document.getElementById('signin-container').style.display = 'none';
+      document.getElementById('todo-container').style.display =
+      'block';
+      document.getElementById('response-message').innerHTML = `Logged in successfully. <a href="#" id="logout-link">Logout</a>`;
+      loadTodos();
+   }else{
+      document.getElementById('response-message').innerText = result.message || 'signIn failed';
+   }
+ } catch (error) {
+   document.getElementById('response-message').innerText = 'Error during signIn'
+ }
+ })
 // Todo on form submission
 
 // Load Todos
