82ecbfd12eb39dd129642e13a3a0cb2aeeff236d
itssunnyfunny <sunnyfunny4062@gmail.com>
2024-12-26 12:28:18 +0530

    deleting a bookmark of given id

--- week-6/6.2-bookmark-manager/backend/routes/bookmarks.js
+++ week-6/6.2-bookmark-manager/backend/routes/bookmarks.js
@@ -17,7 +17,20 @@ export async function addBookmark(req,res,next){
 }
 
 export async function deleteBookmark(req,res,next){
-// write here
+ try {
+    const {id} = req.params;
+    if (!id) {
+     return res.status(401).json({error: "please provide id to delete the bookmark"})
+    };
+    const bookmarkIndex = bookmarks.findIndex(bookmark => bookmark.id == id);
+    if (bookmarkIndex === -1) {
+     return res.status(404).json({error: "Bookmark not found for the given id"})
+    }
+    bookmarks.splice(bookmarkIndex,1);
+    res.json({message: "bookmark deleted successfully"});
+ } catch (error) {
+    return res.status(500).json({error: "an error occured during deleting bookmark !"})
+ }
 }
 
 export async function getAllBookmarks(req,res,next){
