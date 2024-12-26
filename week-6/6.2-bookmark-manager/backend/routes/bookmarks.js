3d0c83a7d7741fc9d46b8d321097b736f0a8365c
itssunnyfunny <sunnyfunny4062@gmail.com>
2024-12-26 12:20:59 +0530

    route for creating new bookmark

--- week-6/6.2-bookmark-manager/backend/routes/bookmarks.js
+++ week-6/6.2-bookmark-manager/backend/routes/bookmarks.js
@@ -1,7 +1,19 @@
-let bookmarks = []; // in memory space
+let bookmarks = [];
+let currentId = 1;
+
 
 export async function addBookmark(req,res,next){
-// write here
+  try {
+    const { category, url} = req.body;
+         if (!category || !url) {
+            return res.status(400).json({error: "Category and url is required"})
+         }
+    const newBookmark = {id: currentId++, category, url}
+    bookmarks.push(newBookmark);
+    res.json(newBookmark)
+  } catch (error) {
+    res.status(500).json({error: "an Error occured during adding a new Bookmark"})
+  }
 }
 
 export async function deleteBookmark(req,res,next){
