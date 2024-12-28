b9bc8422d52fa7b494108dfd65babf6e42113abd
itssunnyfunny <sunnyfunny4062@gmail.com>
2024-12-28 11:21:22 +0530

    fetching todo and rendering to the dom

--- week-6/6.1-todo/frontend/script.js
+++ week-6/6.1-todo/frontend/script.js
@@ -2,17 +2,38 @@ const API_URL = 'http://localhost:3001/todos';
 
 // Fetch existing todos when the page loads
 document.addEventListener('DOMContentLoaded', () => {
-  // fetch todos
+   fetchTodos()
 });
 
 // Fetch todos from backend
 function fetchTodos() {
-    //  write here
+    fetch(API_URL)
+    .then((todo)=> todo.json())
+    .then((todos)=>{
+        todos.forEach(todo => addTodoToDOM(todo));
+    })
+    .catch(error => console.error("error during fetching todos",error))
 }
 
 // Add a new todo to the DOM
 function addTodoToDOM(todo) {
-    //  write here
+   const todoList = document.getElementById('todo-list');
+
+   const todoItem = document.createElement('li')
+   todoItem.classList.add('todo-item')
+   todoItem.setAttribute('todo-id', todo.id);
+
+   const title = document.createElement('span');
+   title.textContent = todo.task;
+
+   const deleteButton = document.createElement('button')
+   deleteButton.textContent = 'delete'
+   deleteButton.addEventListener('click', ()=> deleteTodo(todo.id))
+
+   todoItem.appendChild(title)
+   todoItem.appendChild(deleteButton)
+
+   todoList.appendChild(todoItem)
 }
 
 // Add a new todo
