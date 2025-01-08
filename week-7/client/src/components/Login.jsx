49e2629b9b505e3e6ece3d37e1a5ff25f370284d
itssunnyfunny <sunnyfunny4062@gmail.com>
2025-01-08 14:19:43 +0530

    error and message handling

--- week-7/client/src/components/Login.jsx
+++ week-7/client/src/components/Login.jsx
@@ -5,11 +5,25 @@ import React from 'react'
 const Login = () => {
     const [username, setUsername] = useState("")
     const [password, setPassword] = useState("")
+    const [error, setError] = useState("");
+    const [message, setMessage] = useState("");
+
+
     async function handleLogin() {
+      try {
         const resposne = await axios.post('http://localhost:3000/user/signup',{username: username, password: password});
+        const {token, message} = resposne.data;
+         setMessage(message)
+        localStorage.setItem('token', token);
+      } catch (error) {
+          setError(error.resposne?.data?.message || "something went wrong");
+      }
+
     }
     return (
         <div>
+            {error && <div> {error} </div>}
+            {message && <div> {message} </div>}
             <div>
             <form onSubmit={handleLogin}>
         <div style={{ marginBottom: '10px' }}>
