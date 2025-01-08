52af05c7ea34a4d6811fc29cd40626115bd73ffa
itssunnyfunny <sunnyfunny4062@gmail.com>
2025-01-08 13:50:13 +0530

    login page for user

--- week-7/client/src/components/Login.jsx
+++ week-7/client/src/components/Login.jsx
@@ -3,12 +3,45 @@ import axios from 'axios';
 import React from 'react'
 
 const Login = () => {
-    // call the functions onClick of button.
+    const [username, setUsername] = useState("")
+    const [password, setPassword] = useState("")
     async function handleLogin() {
-        const resposne = await axios.post(); // // if you don't know about axios, give it a read https://axios-http.com/docs/intro
+        const resposne = await axios.post('http://localhost:3000/user/signup',{username: username, password: password});
     }
     return (
-        <div>Login</div>
+        <div>
+            <div>
+            <form onSubmit={handleLogin}>
+        <div style={{ marginBottom: '10px' }}>
+          <label>Username:</label>
+          <input
+            type="text"
+            name="username"
+            value={username}
+            onChange={(e)=>setUsername(e.target.value)}
+            required
+            style={{ width: '100%', padding: '8px', margin: '5px 0' }}
+          />
+        </div>
+
+        <div style={{ marginBottom: '10px' }}>
+          <label>Password:</label>
+          <input
+            type="password"
+            name="password"
+            value={password}
+            onChange={(e)=>setPassword(e.target.value)}
+            required
+            style={{ width: '100%', padding: '8px', margin: '5px 0' }}
+          />
+        </div>
+
+        <button type="submit" style={{ padding: '10px 20px', backgroundColor: '#007BFF', color: '#fff', border: 'none', borderRadius: '5px', cursor: 'pointer' }}>
+          login
+        </button>
+      </form>
+            </div>
+        </div>
     )
 }
 
