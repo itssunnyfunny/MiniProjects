3f6842096e56c50d75cce9351790438b2d7277c4
itssunnyfunny <sunnyfunny4062@gmail.com>
2025-01-10 11:51:06 +0530

    getting all the courses on the frontend

--- week-7/client/src/components/Courses.jsx
+++ week-7/client/src/components/Courses.jsx
@@ -1,10 +1,36 @@
 // courses code here
+import axios from 'axios'
 import React from 'react'
 
-// use axios here, similar to register and login
+
 const Courses = () => {
+  const [courses, setCourses] = useState([]); 
+  const [message, setMessage] = useState("")
+
+  async function getCourses() {
+    const response = await axios.get('http://localhost:3000/users/courses')
+    const {course,message} = response.data;
+     setCourses(course);
+     setMessage(message);
+  }
+
+
   return (
-    <div>Courses</div>
+    <div>
+      {courses.map((course)=>{
+        <div>
+          <div>
+            <img src={course.imageLink} alt={course.imageLink} />
+          </div>
+          <h1>{course.title}</h1>
+          <h2>
+            {course.description}
+          </h2>
+          <div>
+          </div>
+        </div>
+      })}
+    </div>
   )
 }
 
