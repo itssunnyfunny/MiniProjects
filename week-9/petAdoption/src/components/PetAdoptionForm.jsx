f692d6d89ca095cdbaa1a2ee3185e124749b9ae6
itssunnyfunny <sunnyfunny4062@gmail.com>
2025-01-19 12:05:27 +0530

    all the states to manage the form

--- week-9/petAdoption/src/components/PetAdoptionForm.jsx
+++ week-9/petAdoption/src/components/PetAdoptionForm.jsx
@@ -1,9 +1,43 @@
-import React from 'react'
+import React,{useState} from 'react'
+import AdopterData from './AdopterData'
+import { validation } from '../utils/validation'
 
 const PetAdoptionForm = () => {
-  return (
-    <div>PetAdoptionForm</div>
-  )
+ const [formData, setFormData] = useState([])
+ const [values, setValues] = useState({
+  petName: "",
+  petType: "Dog",
+  breed: "",
+  adopterName: "",
+  email: "",
+  phone: ""
+ });
+ 
+ const [showTable, setShowTable] = useState(false)
+ // destructuring thee values object
+ const {petName, petType, breed, adopterName, email, phone} = values;
+
+ const [errors, setErrors] = useState({
+  petName: "",
+  petType: "",
+  breed: "",
+  adopterName: "",
+  email: "",
+  phone: ""
+ })
+
+
+   if (!showTable) {
+    return (
+      <div>
+        <div>
+
+        </div>
+      </div>
+    )
+   }
+  return <AdopterData></AdopterData>
+  
 }
 
 export default PetAdoptionForm
